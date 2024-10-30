86384bacd63a85c20a48d0f5be11852fb8ee8d40
Francisco Hauva <phauva@proton.me>
2024-10-30 20:29:50 -0300

    fix: obtain client ip from correct cloudflare header

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import { getConnInfo } from "hono/bun";
 
 export function getAddressFromContext(c: Context) {
 	return (
-		c.req.header("x-cl") ||
+		c.req.header("cf-connecting-ip") ||
 		c.req.header("x-forwarded-for") ||
 		getConnInfo(c).remote.address
 	);
