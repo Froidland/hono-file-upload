c5f95053ab162da0104e89243fe2ff0a73ef599d
Francisco Hauva <phauva@proton.me>
2024-11-21 03:08:32 -0300

    feat: docker restart unless stopped

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 source .env
 
-docker run -d --name files -p $PORT:3000 --env-file .env -e PORT=3000 --mount type=bind,source=$(pwd)/files,destination=/home/bun/app/files files
+docker run -d --name files -p $PORT:3000 --env-file .env -e PORT=3000 --restart unless-stopped --mount type=bind,source=$(pwd)/files,destination=/home/bun/app/files files
