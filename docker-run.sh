c2f6ce721a153aa543f75779e8b7c83daa433da4
Francisco Hauva <phauva@proton.me>
2024-11-22 21:50:26 -0300

    fix: I hate windows

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,15 @@
-source .env
+#!/bin/bash
 
-docker run -d --name files -p $PORT:3000 --env-file .env -e PORT=3000 --restart unless-stopped --mount type=bind,source=$(pwd)/files,destination=/home/bun/app/files files
+if test -f .env; then
+	source .env
+fi
+
+docker run \
+	-d \
+	--name files \
+	-p 127.0.0.1:${PORT:-4000}:3000 \
+	--env-file .env \
+	-e PORT=3000 \
+	--restart unless-stopped \
+	--mount type=bind,source=$(pwd)/files,destination=/home/bun/app/files \
+	files
